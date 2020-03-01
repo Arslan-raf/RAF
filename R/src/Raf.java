ec80e3c9078806b0402cde965f939084b8252d38
User <User@WIN-H8M92066B6U>
2020-03-01 18:32:13 +0500

    поменял намбер зачётки

--- R/src/Raf.java
+++ R/src/Raf.java
@@ -9,7 +9,7 @@
 	g.fillRect(0,0,d.width,d.height);
 	g.setColor(Color.black);
 	g.setFont(f);
-	drawCenteredString("1234567", d.width, d.height, g);
+	drawCenteredString("18130319", d.width, d.height, g);
 	g.drawRect(0,0,d.width-1,d.height-1);
 	}
 
